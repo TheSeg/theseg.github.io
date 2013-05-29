a8dcd812029ed14710aa07adf366b34be0f5b79c
Seg <seg@segonmedia.com>
2013-05-28 21:25:41 -0700

    Don't show tooltip when platform isn't enabled.

--- js/seg_resume.js
+++ js/seg_resume.js
@@ -8,37 +8,37 @@ $(document).ready(function() {
         'html':true,
         'title':"Developed for but not released on Nintendo Wii.",
     });
-    $(".platform-win").tooltip({
+    $(".platform-win:not(.platform-not)").tooltip({
         'placement':'bottom',
         'html':true,
         'title':"Microsoft Windows<br>Self publish, Steam, &amp; Retail Disc",
     });
-    $(".platform-macosx, .platform-mac").tooltip({
+    $(".platform-macosx:not(.platform-not), .platform-mac:not(.platform-not)").tooltip({
         'placement':'bottom',
         'html':true,
         'title':"Mac OS X<br>Self publish, Steam, &amp; Retail Disc",
     });
-    $(".platform-xbox360").tooltip({
+    $(".platform-xbox360:not(.platform-not)").tooltip({
         'placement':'bottom',
         'html':true,
         'title':"Microsoft Xbox 360<br>XBLA &amp; Retail Disc",
     });
-    $(".platform-ps3").tooltip({
+    $(".platform-ps3:not(.platform-not)").tooltip({
         'placement':'bottom',
         'html':true,
         'title':"Sony PlayStation 3<br>PSN &amp; Retail Disc",
     });
-    $(".platform-wii").tooltip({
+    $(".platform-wii:not(.platform-not)").tooltip({
         'placement':'bottom',
         'html':true,
         'title':"Nintendo Wii<br>WiiWare &amp; Retail Disc",
     });
-    $(".platform-ios").tooltip({
+    $(".platform-ios:not(.platform-not)").tooltip({
         'placement':'bottom',
         'html':true,
         'title':"iOS<br>iPhone &amp; iPad",
     });
-    $(".platform-steam").tooltip({
+    $(".platform-steam:not(.platform-not)").tooltip({
         'placement':'bottom',
         'html':true,
         'title':"Steam &amp; Steamworks<br>for Windows &amp; Mac OS X",
