acbb7f10d5ae7d33f3b6b28df37d76ec4ab406cb
Seg <seg@segonmedia.com>
2013-05-30 22:57:42 -0700

    Smooth Scrolling works.

--- js/src/seg_resume.js
+++ js/src/seg_resume.js
@@ -107,6 +107,22 @@ $(document).ready(function() {
             'html':true,
         });
     });
+    
+    $('.nav-pills a, .toTop').click(function(){
+        var scrollTo = 0;
+        if ( $.attr(this, 'href') === "#introduction") {
+            scrollTo = ($( "#navigation" ).offset().top)-30;
+        } else if ( $.attr(this, 'href') === "#top") {
+            scrollTo = 0;
+        } else {
+            scrollTo = (($( $.attr(this, 'href') ).offset().top)-70);
+        }
+        $('html, body').animate({
+            scrollTop: scrollTo
+        }, 500);
+        return false;
+    });
+    
     load_Twitter();
     load_GooglePlus();
     load_Facebook();
