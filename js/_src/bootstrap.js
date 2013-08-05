f871dc63d5398a97c43fb5b09a96afc8ae1dbe70
Seg <seg@segonmedia.com>
2013-08-05 01:08:31 -0700

    Bootstrap JavaScript set for v2.3.2.

--- js/_src/bootstrap.js
+++ js/_src/bootstrap.js
@@ -1,16 +1,16 @@
 /*
 Code Kit Appends
-@codekit-append "../../bower_components/bootstrap/js/transition.js";
-@codekit-append "../../bower_components/bootstrap/js/alert.js";
-@codekit-append "../../bower_components/bootstrap/js/button.js";
-@codekit-append "../../bower_components/bootstrap/js/carousel.js";
-@codekit-append "../../bower_components/bootstrap/js/dropdown.js";
-@codekit-append "../../bower_components/bootstrap/js/collapse.js";
-@codekit-append "../../bower_components/bootstrap/js/modal.js";
-@codekit-append "../../bower_components/bootstrap/js/tooltip.js";
-@codekit-append "../../bower_components/bootstrap/js/popover.js";
-@codekit-append "../../bower_components/bootstrap/js/scrollspy.js";
-@codekit-append "../../bower_components/bootstrap/js/tab.js";
-@codekit-append "../../bower_components/bootstrap/js/typeahead.js";
-@codekit-append "../../bower_components/bootstrap/js/affix.js";
+@codekit-append "../../bower_components/bootstrap/js/bootstrap-transition.js";
+@codekit-append "../../bower_components/bootstrap/js/bootstrap-alert.js";
+@codekit-append "../../bower_components/bootstrap/js/bootstrap-button.js";
+@codekit-append "../../bower_components/bootstrap/js/bootstrap-carousel.js";
+@codekit-append "../../bower_components/bootstrap/js/bootstrap-dropdown.js";
+@codekit-append "../../bower_components/bootstrap/js/bootstrap-collapse.js";
+@codekit-append "../../bower_components/bootstrap/js/bootstrap-modal.js";
+@codekit-append "../../bower_components/bootstrap/js/bootstrap-tooltip.js";
+@codekit-append "../../bower_components/bootstrap/js/bootstrap-popover.js";
+@codekit-append "../../bower_components/bootstrap/js/bootstrap-scrollspy.js";
+@codekit-append "../../bower_components/bootstrap/js/bootstrap-tab.js";
+@codekit-append "../../bower_components/bootstrap/js/bootstrap-typeahead.js";
+@codekit-append "../../bower_components/bootstrap/js/bootstrap-affix.js";
 */
