30be5380576cc809f2dd976349ffd6cde0cc1683
Seg <seg@segonmedia.com>
2013-08-04 22:17:12 -0700

    Bower: Bootstrap - JavaScript

---
+++ js/_src/bootstrap.js
@@ -0,0 +1,16 @@
+/*
+Code Kit Appends
+@codekit-append "../../bower_components/bootstrap/js/transition.js";
+@codekit-append "../../bower_components/bootstrap/js/alert.js";
+@codekit-append "../../bower_components/bootstrap/js/button.js";
+@codekit-append "../../bower_components/bootstrap/js/carousel.js";
+@codekit-append "../../bower_components/bootstrap/js/dropdown.js";
+@codekit-append "../../bower_components/bootstrap/js/collapse.js";
+@codekit-append "../../bower_components/bootstrap/js/modal.js";
+@codekit-append "../../bower_components/bootstrap/js/tooltip.js";
+@codekit-append "../../bower_components/bootstrap/js/popover.js";
+@codekit-append "../../bower_components/bootstrap/js/scrollspy.js";
+@codekit-append "../../bower_components/bootstrap/js/tab.js";
+@codekit-append "../../bower_components/bootstrap/js/typeahead.js";
+@codekit-append "../../bower_components/bootstrap/js/affix.js";
+*/
