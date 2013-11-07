ecc8e2ebe5ef4a315bb020f714733320784a459e
Seg <seg@segonmedia.com>
2013-11-06 17:40:07 -0800

    Grunt: Using indivdial NPM Tasks
    * Updates compile of files due to updated NPM tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -131,7 +131,13 @@ module.exports = function(grunt) {
   });
   
   // Load the plugins for tasks.
-  grunt.loadNpmTasks('grunt-contrib');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   
   // Inital Setup Task
   grunt.registerTask( 'init', [ 'init' , 'build' ] );
